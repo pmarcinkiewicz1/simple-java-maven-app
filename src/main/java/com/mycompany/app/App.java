791e28008e8e63f74376df7790cb6de9df5633ce
Przemyslaw Marcinkiewicz -X (prmarcin - CODILIME SP ZOO at Cisco) <prmarcin@cisco.com>
2020-11-30 03:14:09 -0800

    Fix

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        Systtem.out.println("Hello World2!!");
+        System.out.println("Hello World2!!");
     }
 
     private final String getMessage() {
